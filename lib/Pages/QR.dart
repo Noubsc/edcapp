c74a8689df0268946207e0614ea001f730bc9a6e
Noubsc <noultm@gmail.com>
2022-06-18 16:25:28 +0700

    fish

--- lib/Pages/QR.dart
+++ lib/Pages/QR.dart
@@ -19,21 +19,28 @@ class QrPage extends StatelessWidget {
           children: [
             Text(
               "Mini Big C ສາຂາ ດົງໂດກ", //this.qrstring.toString(),
-              style: TextStyle(fontSize: 24.0),
+              style: TextStyle(
+                fontSize: 24.0,
+                color: Colors.green,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-            ClipRRect(
-              borderRadius: BorderRadius.circular(1.0),
+            Container(
+              margin: const EdgeInsets.all(15.0),
+              padding: const EdgeInsets.all(3.0),
+              decoration: BoxDecoration(
+                  border: Border.all(width: 2, color: Mycolor.mainColor)),
               child: QrImage(
                   data:
                       "0002010102125208MERCHANT5303418550560000560105702005802LA5932adc3a3358d1a4c0482d88f356d3b9ba46009Vientiane81020063045fa3", //this.qrstring.toString(),
                   version: QrVersions.auto,
-                  size: 300.0,
+                  size: 350.0,
                   gapless: false,
                   embeddedImage: AssetImage('assets/images/mmoney.png'),
                   embeddedImageStyle: QrEmbeddedImageStyle(
-                    size: Size(50, 50),
+                    size: Size(40, 40),
                   )),
-            )
+            ),
           ],
         ),
       ),
